dbfb11e1a3e74e02e6b9283cc11527b60f27937d
Tyler Scott <tylerclaytonscott@gmail.com>
2024-11-22 21:02:00 -0500

    Fix subtraction logic

--- javascript.js
+++ javascript.js
@@ -51,7 +51,7 @@ function add(num1,num2){
     return num1+num2;
 }
 function subtract(num1,num2){
-    return num1-num2;
+    return num2-num1;
 }
 function multiply(num1,num2){
     return num1 * num2;
